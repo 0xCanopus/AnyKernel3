cfbe4cd7e670426a21884380b7903f29eeff836a
0xCanopus <saqibgamer307@gmail.com>
2026-03-25 15:38:10 +0500

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=FSociety kernel by Richard
+kernel.string=Quantum by Canopus
 do.devicecheck=1
 do.modules=0
 do.systemless=0
